b09f8503a20dae2e46ef536c9a0d9136f9a16157
abhi-aro <abhiaro27@gmail.com>
2022-11-10 12:00:50 +0530

    Backend Basics Ready

--- routes/exercises.js
+++ routes/exercises.js
@@ -2,20 +2,48 @@ const router = require("express").Router();
 let User = require("../models/exercise.model");
 
 router.route("/").get((req, res) => {
-	User.find()
-		.then((exercises) => res.json(exercises))
-		.catch((err) => res.status(400).json("Error: " + err));
+  User.find()
+    .then((exercises) => res.json(exercises))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").get((req, res) => {
+  User.findById(req.params.id)
+    .then((exercise) => res.json(exercise))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").delete((req, res) => {
+  User.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Exercise Deleted"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+  User.findByIdAndUpdate(req.params.id)
+    .then((exercise) => {
+      exercise.username = req.body.username;
+      exercise.description = req.body.description;
+      exercise.duration = req.body.duration;
+      exercise.date = req.body.date;
+
+      exercise
+        .save()
+        .then(() => res.json("Exercise Updated!"))
+        .catch((err) => res.status(400).json("Error" + err));
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 router.route("/add").post((req, res) => {
-	const username = req.body.username;
-	const description = req.body.description;
-	const duration = Number(req.body.duration);
-	const date = Date.parse(req.body.date);
-	const newExercise = new User({ username, description, duration, date });
-	newExercise
-		.save()
-		.then(() => res.json("Exercise added!"))
-		.catch((err) => res.status(400).json("Error: " + err));
+  const username = req.body.username;
+  const description = req.body.description;
+  const duration = Number(req.body.duration);
+  const date = Date.parse(req.body.date);
+  const newExercise = new User({ username, description, duration, date });
+  newExercise
+    .save()
+    .then(() => res.json("Exercise added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 module.exports = router;
