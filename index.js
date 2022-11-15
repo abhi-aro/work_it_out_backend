6120fd83da05d73b1f8aa392eb87c3fa4a4d9d4e
Abhishek <abhiaro27@gmail.com>
2022-11-16 00:00:42 +0530

    Cors error fix 2

--- index.js
+++ index.js
@@ -7,13 +7,7 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(cors());
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors({origin: "*"}));
 
 app.use(express.json());
 
