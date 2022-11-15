b72c9c9722cb3e7fbc2dd56af4be2d5a69528b1f
Abhishek <abhiaro27@gmail.com>
2022-11-15 23:42:08 +0530

    bug fix 1 for cors error

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
